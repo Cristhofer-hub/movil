5019c7a0c97f3818bfa8588c8bc08f9fe23d3492
Cristhofer-hub <cristhofer.parreno@istpet.edu.ec>
2025-05-08 11:18:19 -0500

    AplicacionesTERCERO

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Mundo</Text>
+      <Image style={styles.imagen} source={{uri:"https://istpetonline.edu.ec/pluginfile.php/1/theme_klassroom/featuredpostimage/1720643456/on-line.png"}}/>
     </View>
   );
 }
@@ -15,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imagen: {
+    width:250,
+    height:150
+  }
 });
